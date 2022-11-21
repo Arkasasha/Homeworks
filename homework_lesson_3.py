c759096adb7d1ff69f2588f2e3ba1e32224f17c3
Arkasasha <117239939+Arkasasha@users.noreply.github.com>
2022-11-22 00:10:44 +0200

    Add files via upload

---
+++ homework_lesson_3.py
@@ -0,0 +1,36 @@
+def maximum(num_1, num_2):
+    return max(num_1, num_2)
+
+
+def minimum(num_1, num_2, num_3):
+    return min(num_1, num_2, num_3)
+
+
+def absolute_value(num_abs):
+    return abs(num_abs)
+
+
+def summarize(num_1, num_2):
+    print(num_1 + num_2)
+
+
+def number_symbol(num):
+    if num > 0:
+        print('This number is positive')
+    elif num == 0:
+        print('This number equal to zero')
+    else:
+        print('This number is negative')
+
+
+if __name__ == '__main__':
+    NUMBER_1 = 5
+    NUMBER_2 = 10
+    NUMBER_3 = 1
+    NUMBER_ABS = -7.089
+
+    print(maximum(NUMBER_1, NUMBER_2))
+    print(minimum(NUMBER_1, NUMBER_2, NUMBER_3))
+    print(absolute_value(NUMBER_ABS))
+    summarize(NUMBER_1, NUMBER_2)
+    number_symbol(NUMBER_ABS)
---
+++ homework_lesson_5.py
@@ -0,0 +1,70 @@
+import random
+
+
+def retry(attempts=5, desired_value=None):
+    def wrapper(func):
+        def inner_wrapper(*args, **kwargs):
+            nonlocal attempts
+            nonlocal desired_value
+            result = func(*args, **kwargs)
+            if not result == desired_value:
+                attempts = attempts - 1
+                if attempts > 0:
+                    inner_wrapper(*args, **kwargs)
+                else:
+                    print("The desired value wasn't found")
+                    return result
+            return result
+
+        return inner_wrapper
+
+    return wrapper
+
+
+@retry(desired_value=3)
+def get_random_value():
+    return random.choice((1, 2, 3, 4, 5))
+
+
+@retry(desired_value=[1, 2])
+def get_random_values(choices, size=2):
+    return random.choices(choices, k=size)
+
+
+@retry(attempts=7, desired_value=3)
+def get_random_value_2():
+    return random.choice((1, 2, 3, 4, 5))
+
+
+@retry(attempts=2, desired_value=[1, 2, 3])
+def get_random_values_2(choices, size=2):
+    return random.choices(choices, k=size)
+
+
+def print_a_rows(side, amount):
+    row = '*' + ' '*(side-2) + '*'
+    print(row)
+    amount = amount - 1
+    if not amount == 0:
+        print_a_rows(side, amount)
+
+
+def print_square(side):
+    if side == 1:
+        print("*")
+    elif side == 2:
+        print("**")
+        print("**")
+    else:
+        print('*'*side)
+        print_a_rows(side, side-2)
+        print('*'*side)
+
+
+if __name__ == '__main__':
+    get_random_value()
+    get_random_value_2()
+    get_random_values([1, 2, 3, 4])
+    get_random_values_2([1, 2, 3, 4], 3)
+    get_random_values([1, 2, 3, 4], size=1)
+    print_square(4)
